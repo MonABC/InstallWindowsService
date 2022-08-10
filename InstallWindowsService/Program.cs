fa49c30c5832a85ee44e197d4e9c05136b15d824
Viet. Mai Tien - CMC Global DJ2 <mtviet@cmcglobal.vn>
2022-08-10 17:41:40 +0700

    tinh nang sua Recovery Done

--- InstallWindowsService/Program.cs
+++ InstallWindowsService/Program.cs
@@ -13,6 +13,7 @@ namespace InstallWindowsService
         {
             InstallWindowsService();
             //UninstallWindowsService();
+            //SetRecoveryOptions("ServiceName01090822");
         }
 
         public static void InstallWindowsService()
@@ -31,9 +32,31 @@ namespace InstallWindowsService
             process.StartInfo.FileName = "InstallUtil.exe";
             process.StartInfo.WorkingDirectory = @"C:\windows\microsoft.net\framework\v4.0.30319";
             process.StartInfo.Arguments = " -u D:\\Service\\WindowsService01090822\\WindowsService01090822\\bin\\Debug\\WindowsService01090822.exe";
-            process.StartInfo.Verb = "runas";
+            //process.StartInfo.Verb = "runas";
             process.Start();
             process.WaitForExit(60000);
+        }
+
+        static void SetRecoveryOptions(string serviceName)
+        {
+         
+            int exitCode;
+            using (var process = new Process())
+            {
+                var startInfo = process.StartInfo;
+                startInfo.FileName = "sc";
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+                // tell Windows that the service should restart if it fails
+                startInfo.Arguments = string.Format("failure \"{0}\" reset= 0 actions= restart/120000/run//run/", serviceName);
+                process.Start();
+                process.WaitForExit();
+
+                exitCode = process.ExitCode;
+            }
+
+            //if (exitCode != 0)
+            //    throw new InvalidOperationException();
         }
 
 
